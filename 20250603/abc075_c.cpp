d5b4d74e290b4b5ccbbb2e9e992649f3147aca56
1s22s1 <1s22s1@users.noreply.github.com>
2025-06-03 12:18:36 +0900

    :up:

---
+++ 20250603/abc075_c.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n';
+#define ll long long
+
+int init()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    return 0;
+}
+
+class Graph
+{
+
+public:
+    vector<int> visited;
+
+    Graph(vector<vector<int>> graph, int start = 0)
+    {
+        visited.assign(graph.size(), -1);
+
+        queue<int> queue;
+        queue.push(start);
+
+        visited.at(start) = 0;
+
+        while (!queue.empty())
+        {
+            int current = queue.front();
+            queue.pop();
+
+            for (int next : graph.at(current))
+            {
+                if (visited.at(next) != -1)
+                {
+                    continue;
+                }
+
+                queue.push(next);
+                visited.at(next) = visited.at(current) + 1;
+            }
+        }
+    }
+};
+
+int main()
+{
+    init();
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<pair<int, int>> ab;
+    for (auto _ : ranges::views::iota(0, M))
+    {
+        int a, b;
+        cin >> a >> b;
+
+        ab.push_back({a, b});
+    }
+
+    auto answer = 0;
+
+    for (int i = 0; i < M; i++)
+    {
+        vector<vector<int>> g(N);
+        for (int j = 0; j < M; j++)
+        {
+            if (i != j)
+            {
+                g.at(ab.at(i).first - 1).push_back(ab.at(i).second - 1);
+                g.at(ab.at(i).second - 1).push_back(ab.at(i).first - 1);
+            }
+        }
+
+        Graph graph(g);
+        auto visited = graph.visited;
+
+        if (any_of(visited.begin(), visited.end(), [](int n)
+                   { return n == -1; }))
+        {
+            answer++;
+        }
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
